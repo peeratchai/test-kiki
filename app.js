27ec8a900faf8f0b5515213ef49b15d18c3f918a
peeratchai kittisupap <peeratck@scg.com>
2020-10-22 16:39:04 +0700

    second commit

--- app.js
+++ app.js
@@ -21,12 +21,31 @@ app.set("view engine", "jade")
 
 app.use(logger("dev"))
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", indexRouter)
 app.use("/users", usersRouter)
+
+app.get('/jobs', async (req, res) => {
+  try {
+    let { description = '', full_time, location = '', page = 1 } = req.query;
+
+    description = description ? encodeURIComponent(description) : '';
+    location = location ? encodeURIComponent(location) : '';
+    full_time = full_time === 'true' ? '&full_time=true' : '';
+    if (page) {
+      page = parseInt(page);
+      page = isNaN(page) ? '' : `&page=${page}`;
+    }
+    const query = `https://jobs.github.com/positions.json?description=${description}&location=${location}${full_time}${page}`;
+    const result = await axios.get(query);
+    res.send(result.data);
+  } catch (error) {
+    res.status(400).send('Error while getting list of jobs.Try again later.');
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
